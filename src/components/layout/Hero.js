5dd8d725b153294a94ef9245b6618a39afe1ef0b
Yonas Mekonnen Feshaye <yonas.m.feshaye@hotmail.com>
2023-11-15 22:54:18 -0500

    modified the Hero file

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -2,24 +2,27 @@ import Image from "next/image";
 import Right from "../icons/Right";
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2">
-      <div>
-         <h1 className="text-4xl font-semibold">Crafted meals for foodies.</h1>
-         <p className="mt-4">
+    <section className="hero">
+      <div className="py-12 pr-6">
+         <h1 className="text-4xl font-semibold">Crafted <br/>meals for <br /><span className="text-primary">foodies</span></h1>
+         <p className="mt-4 text-secondary-200 text-sm">
             Embark on a taste adventure at our charming cafe, offering     breakfast delights, paninis, pizzas, wraps, and salads—all crafted     with artisanal expertise. From the morning sun to savory bites, we     infuse every dish with a burst of flavor, ensuring a brief escape     to culinary paradise.{" "}
          </p>
-         <div className="flex gap-4">
-            <button className="bg-primary flex gap-2 text-white px-4 py-2 rounded-full">
+         <div className="flex gap-4 items-center text-sm font-semibold pt-2">
+            <button className="bg-primary flex gap-2 uppercase text-white px-4 py-2 rounded-full items-center">
                 Order now
                 <Right />
                 
             </button>
-            <button>Learn more</button>
+            <button className="flex gap-2 text-secondary-200 ">
+                Learn more
+                <Right />
+            </button>
          </div>
       </div>
       
-      <div className="w-16 h-16 relative">
-         <Image src={'/breakfast.jpg'} layout="fill" objectFit="contain"    alt={"breakfast"}/>
+      <div className="relative ">
+         <Image src={'/breakfast.jpg'} layout="fill" objectFit="contain" alt={"breakfast"} className="rounded-full"/>
       </div>
       
     </section>
